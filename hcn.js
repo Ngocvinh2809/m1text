e233f4b54137ce26df4a938a7008c1c7034f9a2e
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-05-12 14:16:43 +0700

    update

---
+++ hcn.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+    inputwidth = prompt("Nhập chiều dài");
+    inputheight = prompt("Nhập chiều rộng") ;
+    let width = parseInt(inputwidth);
+    let height = parseInt(inputheight);
+    let tổng = width * height ;
+    document.write("tổng :" + tổng);
---
+++ java.js
@@ -0,0 +1,7 @@
+var intro = "vinh";
+    document.getElementById('intro').innerHTML = intro;
+
+
+
+
+    
